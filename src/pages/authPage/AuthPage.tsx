410b635ac57e1183f2341cf377f30399512c4ba4
Wiphaphon Somkhit <wiphaphon.s@iconkaset.com>
2022-07-21 09:26:44 +0700

    fix login full screen

--- src/pages/authPage/AuthPage.tsx
+++ src/pages/authPage/AuthPage.tsx
@@ -13,24 +13,21 @@ export const AuthPage: React.FC = () => {
       <div className="row">
         <div className="col-lg-6">
           <div className="d-flex justify-content-start">
-            <img alt="imageLogin" src={image.login} width={"100%"} />
+            <img alt="imageLogin" src={image.login} className="w-100 vh-100" />
           </div>
         </div>
         <div className="col-lg-6 ">
           <div
             style={{
-              marginTop: "25%",
+              marginTop: "35%",
               paddingLeft: "25%",
               paddingRight: "25%",
             }}
           >
             <div className="text-center">
-              <img alt="logo" src={icon.iconLogin} width={"60%"} />
+              <img alt="logo" src={icon.iconLogin} width={"40%"} />
             </div>
-            <div
-              className="text-center justify-content-center"
-              style={{ marginTop: "3rem" }}
-            >
+            <div className="text-center justify-content-center">
               <FromLogin />
             </div>
           </div>
