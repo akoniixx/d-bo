c974e65da674a96c637af67ce2ad0b6b50fde0e2
Wiphaphon.S <wiphaphon.s@iconkaset.com>
2023-08-28 14:30:52 +0700

    maintenance time to bounce to the login page

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,29 @@
 import axios from "axios";
+import moment from "moment";
+import { convertBuddhistYear } from "../utilities/ConvertToBuddhistYear";
 
-const DEV_URL = 'https://api-dnds-dev.iconkaset.com'
-export const BASE_URL = process.env.NODE_ENV === "development" ? DEV_URL : process.env.REACT_APP_URL_HOST;
+const DEV_URL = "https://api-dnds-dev.iconkaset.com";
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? DEV_URL
+    : process.env.REACT_APP_URL_HOST;
 
 const token = JSON.parse(localStorage.getItem("token") || "{  }");
 
 axios.interceptors.request.use(async (config: any) => {
+  const start: any = localStorage.getItem("MA1");
+  const end: any = localStorage.getItem("MA2");
+
+  const dateNow = convertBuddhistYear.toBuddhistYear(
+    moment(new Date()),
+    "DD MMMM YYYY ช่วงเวลา HH:mm  "
+  );
+
+  if (dateNow >= start && dateNow <= end) {
+    localStorage.clear();
+    sessionStorage.clear();
+    window.location.href = "/";
+  }
   const token = JSON.parse(localStorage.getItem("token") || "{  }");
   config.headers["Authorization"] = `Bearer ${token}`;
   return config;
--- src/datasource/MaintenanceDataSource.ts
+++ src/datasource/MaintenanceDataSource.ts
@@ -2,11 +2,11 @@ import { httpClient, BASE_URL } from "../config/config";
 import { MaintenanceSystem } from "../entities/MaintenanceSystemEntities";
 
 export class MaintenanceDataSource {
-  static getMaintenceSystem(type: string): Promise<MaintenanceSystem> {
+  static getMaintenceSystem(type: string): Promise<any> {
     return httpClient
       .get(BASE_URL + "/ma/system-maintenance/get_notices/?typeService=" + type )
       .then((response) => {
-        return response.data.responseData;
+        return response.data;
       })
       .catch((err) => {
         console.log(err, "err getnotice");
