e3682ca239e88624cae4a70c8a152cd09c5d22a8
Wiphaphon.S <wiphaphon.s@iconkaset.com>
2023-12-17 13:10:48 +0700

    fix mission date

--- src/pages/mission/droner/MissionReport.tsx
+++ src/pages/mission/droner/MissionReport.tsx
@@ -79,10 +79,18 @@ function MissionReport() {
       setCountInpro(res.count)
       const tableList = []
       for (let i = 0; res.data.length > i; i++) {
+        let updateAt = null
+        if (res.data[i].dronerTransactions && res.data[i].dronerTransactions.length > 0) {
+          for (let j = 0; j < res.data[i].dronerTransactions.length; j++) {
+            if (res.data[i].dronerTransactions[j].updateAt) {
+              updateAt = res.data[i].dronerTransactions[j].updateAt
+              break
+            }
+          }
+        }
+
         const table: any = {
-          updateAt: res.data[i].conditionUpdate
-            ? res.data[i].conditionUpdate[`conditionUpdate${num}`]
-            : res.data[i].updateAt,
+          updateAt: updateAt ?? res.data[i].updateAt,
           name: res.data[i].firstname + ' ' + res.data[i].lastname,
           nickname: res.data[i].nickname,
           telephone: res.data[i].telephoneNo,
