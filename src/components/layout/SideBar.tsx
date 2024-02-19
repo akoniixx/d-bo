941a950f9a9a354e1004d4c867e6c3ed5f61cecb
Wiphaphon.S <wiphaphon.s@iconkaset.com>
2024-02-19 10:14:11 +0700

    fixed space

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -38,7 +38,7 @@ export const pathLists = (isAccounting: boolean, isAdminTask?: boolean) => {
             {
               path: '/IndexAdminTask',
               name: 'IndexAdminTask',
-              title: 'แก้ไข/ประวัติงาน',
+              title: 'แก้ไข / ประวัติงาน',
               subMenu: [],
             },
             {
